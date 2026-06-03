5e857b2121671d7575e7a6696b6ced43ad74f460
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-06-02 21:22:52 -0400

    feat: added form validation.
    Users will now receive feedback for incorrect data.

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 app.use("/sign-up", signUpRouter);
 
--- routes/sign-up.js
+++ routes/sign-up.js
@@ -2,8 +2,30 @@ const { Router } = require("express");
 
 const signUpRouter = Router();
 
+function validateInput(req, errors) {
+  Object.keys(req.body).map((key) => {
+    if (req.body[key].length >= 64) {
+      errors[key] = "Length too long";
+    }
+    if (req.body["full-name"].split(" ").length <= 1) {
+      errors["full-name"] = "Enter first and last name separated by spaces";
+    }
+  });
+  return errors;
+}
+
 signUpRouter.get("/", (req, res) => {
-  res.render("sign-up", { title: "Sign-Up" });
+  res.render("sign-up", { title: "Sign-Up", errors: {}, values: {} });
+});
+signUpRouter.post("/", (req, res) => {
+  const errors = {};
+  validateInput(req, errors);
+  if (Object.keys(errors).length === 0) {
+    res.redirect("/");
+  } else {
+    const values = req.body;
+    res.status(422).render("sign-up", { title: "Sign-Up", errors, values });
+  }
 });
 
 module.exports = { signUpRouter };
