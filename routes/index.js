ad519eb94ab3ea3d0efda918df1edd03629f131f
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2026-06-06 23:41:27 -0400

    feat: admins can now delete messages.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const { getMessages } = require("../models/query");
+const { deleteMessage } = require("../models/update");
 
 const indexRouter = Router();
 
@@ -7,6 +8,16 @@ indexRouter.get("/", async (req, res, next) => {
   try {
     const messages = await getMessages();
     res.render("index", { title: "Home", user: req.user, messages });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
+indexRouter.delete("/:time&:title", async (req, res, next) => {
+  try {
+    await deleteMessage(req.params.title, req.params.time);
+    res.json({ redirect: "/" });
   } catch (err) {
     console.log(err);
     next(err);
